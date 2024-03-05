cfd984d5cea842f98829cbb21309d61d5d486eb9
Gopalkrishna <gopalkrishnabhati1@gmail.com>
2024-03-05 19:24:28 +0530

    Refactors code

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,14 +8,6 @@ class StringCalculator
       numbers = numbers[4..-1]
     end
 
-    compute_numbers = numbers.split("#{delimiter}").map do |elem| 
-      if elem.include?("\n")
-        elem.split("\n")
-      else
-        elem
-      end
-    end
-
-    compute_numbers.flatten.map(&:to_i).sum
+    numbers.split(/#{delimiter}|\n/).map(&:to_i).sum
   end
 end
