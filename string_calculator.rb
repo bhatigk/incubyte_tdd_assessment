534cbbfb8d31408c3d3aaf4e20516f74b25238a1
Gopalkrishna <gopalkrishnabhati1@gmail.com>
2024-03-05 19:18:05 +0530

    Implement the string calculator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,21 @@
 class StringCalculator
   def self.add(numbers)
+    raise "negative numbers not allowed: #{numbers.scan(/-\d+/).join(', ')}" if numbers.include?('-')
+
+    delimiter = ','
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..-1]
+    end
+
+    compute_numbers = numbers.split("#{delimiter}").map do |elem| 
+      if elem.include?("\n")
+        elem.split("\n")
+      else
+        elem
+      end
+    end
+
+    compute_numbers.flatten.map(&:to_i).sum
   end
 end
-
-
